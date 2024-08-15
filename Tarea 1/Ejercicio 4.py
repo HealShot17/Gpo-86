503854b498ace3de4815fedd1b067a48f108c891
HealShot17 <albertokcrs@outlook.com>
2024-08-15 14:48:46 -0500

    Ejercicio 4

--- Tarea 1/Ejercicio 4.py
+++ Tarea 1/Ejercicio 4.py
@@ -1,2 +1,13 @@
 """ Escribir una función que tome un carácter y devuelva True si es una vocal,
 de lo contrario devuelve False."""
+
+lista = ['a','e','i','o','u']
+
+Caracter = str(input("Ingrese un carcter"))
+
+if Caracter in lista:
+    print("True")
+    
+else:
+    print("False")
+    
