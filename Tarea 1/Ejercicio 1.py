c4b66bd9f03d7175e17aca2baf1258342ffbe9b0
Alberto Caceres Ortega <albertokcrs@outlook.com>
2024-08-15 13:33:02 -0600

    Actualizar Ejercicio 1.py
    prueba 1.3

--- Tarea 1/Ejercicio 1.py
+++ Tarea 1/Ejercicio 1.py
@@ -1,3 +1,10 @@
 """Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos.
 (Es cierto que Python tiene la función max() incorporada, pero hacerla nosotros mismos es 
  muy buen ejercicio)"""
+
+print ("Ingrese 2 números por favor")
+
+if a>b:
+    print("el número más grande es a")
+else: 
+    print("el número más grande es b")
