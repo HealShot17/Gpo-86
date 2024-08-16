13f18507313e19106d4eeebcf6d453c7e69a9945
HealShot17 <albertokcrs@outlook.com>
2024-08-16 14:27:21 -0500

    Prueba ejercicio 8

--- Tarea 1/Ejercicio 8.py
+++ Tarea 1/Ejercicio 8.py
@@ -1,3 +1,14 @@
 """Definir una función superposicion() que tome dos listas y devuelva True si tienen 
 al menos un miembro en común o devuelva True si tiene al menos un miembro en común o devuelva 
 False de lo contrario. Escribir la función usando el bucle for anidado."""
+
+
+lista1=[str(input('Primer caracter por favor ')),str(input('Segundo caracter por favor ')),str(input('Tercer caracter por favor ')),str(input('Cuarto caracter por favor '))]
+lista2=[str(input('Primer caracter por favor ')),str(input('Segundo caracter por favor ')),str(input('Tercer caracter por favor ')),str(input('Cuarto caracter por favor '))]
+
+
+if lista1 in lista2:
+    print("True")
+    
+else:
+    print("False")
