8772c03df431584a26caccb795461d8ab202359b
HealShot17 <albertokcrs@outlook.com>
2024-08-16 14:16:58 -0500

    Ejercicio 6

--- Tarea 1/Ejercicio 6.py
+++ Tarea 1/Ejercicio 6.py
@@ -1,13 +1,9 @@
 """Definir una función inversa() que calcule la inversión de una cadena. 
 Por ejemplo,la cadena "estoy probando" deberia devolver la cadena "odnaborp yotse"""""
 
-lista=[str(input('Primer caracter por favor ')),"S","T","O","Y"," ","P","R","O","B","A","N","D","O"]
+cadena= str(input('Ingrese una cadena de caracteres'))
 x = 0
-for elemento in lista:
-    x= x+1
-for elemnto in lista:
+for elemento in cadena:
     print(elemento)
-while x>0:
-    print(x)
-    x-=1
+
     
