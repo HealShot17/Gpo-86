6d53f3262b62b598827f0acdbde317d364822807
HealShot17 <albertokcrs@outlook.com>
2024-08-18 13:48:48 -0600

    Palindromo

--- Tarea 1/Ejercicio 7.py
+++ Tarea 1/Ejercicio 7.py
@@ -1,2 +1,19 @@
 """Definir una función es palindromo() que reconoce palíndromos (es decir, palabras que tieneen 
 el mismo aspecto escritas invertidas). Ejemolo: es palindromo ("radar") tendría que devolver True"""
+
+entrada_usuario = str(input("Ingrese una palabra: "))
+def es_palindromo(palabra):
+    palabra = palabra.lower()
+    palabra_reversa = palabra[::-1]
+   
+    return palabra == palabra_reversa
+
+
+
+
+resultado = es_palindromo(entrada_usuario)
+
+if resultado:
+    print(f"La palabra '{entrada_usuario}' es un palíndromo.")
+else:
+    print(f"La palabra '{entrada_usuario}' no es un palíndromo.")
