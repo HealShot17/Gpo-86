a8baf435fd4caabd3c1f97f007b92ab816ef6a7c
HealShot17 <albertokcrs@outlook.com>
2024-08-18 13:55:23 -0600

    Ejercicio 9

--- Tarea 1/Ejercicio 9.py
+++ Tarea 1/Ejercicio 9.py
@@ -1,3 +1,15 @@
 """ Definir una funcion generar n caracteres() que tome un entero n y
 devuelva el carácter multiplicado por n.
 Por ejemplo generar n caracteres(5,"x") deberia debolber "xxxxx"."""
+
+n = int(input("Ingrese el número de repeticiones: "))
+
+caracter = "input("Ingrese el carácter a repetir: ")"
+
+def generar_n_caracteres(n, caracter):
+    return caracter * n
+
+
+
+resultado = generar_n_caracteres(n, caracter)
+print(f"La cadena generada es: {resultado}")
