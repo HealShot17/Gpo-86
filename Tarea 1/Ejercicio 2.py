5d4ec5ff21bcd7018694eb4e75811065d0bfa579
HealShot17 <albertokcrs@outlook.com>
2024-08-18 13:19:29 -0600

    Mejora del 2

--- Tarea 1/Ejercicio 2.py
+++ Tarea 1/Ejercicio 2.py
@@ -4,19 +4,19 @@ N1= int (input('Ingrese el primer número por favor: '))
 N2= int (input('Ingrese el segundo número por favor: '))
 N3= int (input('Ingrese el tercer número por favor: '))
 
-def max_de_tres(N1,N2,N3):
+def max_de_tres( Dato1,Dato2,Dato3):
     
-    if N1==N2 and N1==N3:
+    if Dato1==Dato2 and Dato1==Dato3:
         print("Todos los numeros son iguales")
-    elif N2==N1 and N2>N3:
+    elif Dato2==Dato1 and Dato2>Dato3:
         print("Los numeros mas grandes son el primero y el segundo")
-    elif N2==N3 and N2>N1:
+    elif Dato2==Dato3 and Dato2>Dato1:
         print("Los numeros mas grandes son el segundo y el tercero ")
-    elif N1==N3 and N1>N2:
+    elif Dato1==Dato3 and Dato1>Dato2:
         print("Los numeros mas grandes son el pri2mero y el tercero ")
-    elif N1>N3 and N1>N2:
+    elif Dato1>Dato3 and Dato1>Dato2:
         print("El numero mas grande es el primero ")
-    elif N2>N1 and N2>N3:
+    elif Dato2>Dato1 and Dato2>Dato3:
         print("El primero mas grande es el segundo ")
     else: 
         print("el número más grande es el tercero")
