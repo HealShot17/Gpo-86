01e86cb6826d2f23f88864fa2c006e4ece8d0b6e
HealShot17 <albertokcrs@outlook.com>
2024-08-18 15:09:34 -0600

    Acomodo de datos

--- Tarea 1/Ejercicio 10.py
+++ Tarea 1/Ejercicio 10.py
@@ -4,3 +4,16 @@ en la pantalla. Ejemplo: histograma ([4,9,7]) deberia imprimir lo siguiente
  *********
  *******
  """
+#Datos=[int(input("Ingrese el dato 1 del histograma: ")),int(input("Ingrese el dato 2 del histograma: ")),int(input("Ingrese el dato 3 del histograma: "))]
+Tamano = int(input("Ingrese la cantidad de elementos en la lista: "))
+Datos = []
+for i in range(Tamano):
+    elemento = int(input(f"Ingrese el elemento {i+1} de la lista: "))
+    Datos.append(elemento)
+def histograma(lista):
+    for numero in lista:
+        print('*' * numero)
+
+
+histograma(Datos)
+
--- Tarea 1/Ejercicio 8.py
+++ Tarea 1/Ejercicio 8.py
@@ -3,13 +3,14 @@ al menos un miembro en común o devuelva True si tiene al menos un miembro en co
 False de lo contrario. Escribir la función usando el bucle for anidado."""
 
 n1 = int(input("Ingrese la cantidad de elementos en la primera lista: "))
+n2 = int(input("Ingrese la cantidad de elementos en la segunda lista: "))
 lista1 = []
+lista2 = []
 for i in range(n1):
     elemento = input(f"Ingrese el elemento {i+1} de la primera lista: ")
     lista1.append(elemento)
 
-n2 = int(input("Ingrese la cantidad de elementos en la segunda lista: "))
-lista2 = []
+
 for i in range(n2):
     elemento = input(f"Ingrese el elemento {i+1} de la segunda lista: ")
     lista2.append(elemento)
