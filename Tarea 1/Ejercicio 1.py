46e6bf4b17a12cc62360af15138d88f065a1fc12
HealShot17 <albertokcrs@outlook.com>
2024-08-18 12:28:43 -0600

    Ejercicio 1

--- Tarea 1/Ejercicio 1.py
+++ Tarea 1/Ejercicio 1.py
@@ -6,11 +6,12 @@ N1= int (input('Ingrese el primer número por favor: '))
 N2= int (input('Ingrese el segundo número por favor: '))
 N3= ("Ambos numeros son iguales")
 def max(N1,N2):
+    
     if N1==N2:
         return N3
     elif N1>N2:
         return N1
     else: 
         return N2
-    
-print("EL o los datos mayores es",max(N1,N2))
+
+print("EL o los datos mayores son o son",max(N1,N2))
