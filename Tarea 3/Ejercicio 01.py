37b87f43df9fe77d7062ac0469fa3ad65684e3d8
HealShot17 <albertokcrs@outlook.com>
2024-08-21 11:16:27 -0600

    Ejercicio 1 terminado

--- Tarea 3/Ejercicio 01.py
+++ Tarea 3/Ejercicio 01.py
@@ -1,6 +1,6 @@
 """Escribir un programa que solicite al usuario su nombre y edad, y luego imprima un mensaje de bienvenida
 que incluya ambos.
 """
-nombre = (input("Ingrese el nombre de la persona por favor"))
-edad = int(input(f"Ingrese la edad de {nombre} edad por favor"))
-print(f"Bienvenido {nombre} te felicitamos en este pais es complicado cumplir {edad} años ")
+nombre=(input("Ingrese el nombre de la persona por favor: "))
+edad=int(input(f"Ingrese la edad de {nombre} edad por favor: "))
+print(f"Bienvenido {nombre} Te felicitamos en este pais es complicado cumplir {edad} años ")
