c0afa84c70e6484889c488a88f14d61d74853681
HealShot17 <albertokcrs@outlook.com>
2024-08-26 14:44:28 -0500

    Clase listas

--- Hola mundo.py
+++ Hola mundo.py
@@ -1,2 +1,30 @@
 print ('Hola mundo')
 print("Prueba")
+lista=[1,2,3]
+n=len(lista)
+print(n)
+y="Hola"
+
+"""listas
+    lista1 = list()
+    lista2 = []
+for i in range (2):
+    lista1.append(input(f"Ingrese el lugar" {i+1}))
+lista1.remove("6")
+print(lista1.index("luis"))
+
+a = int(input("Ingrese las filas: "))
+b = int(input("Ingrese las columnas: "))
+
+for i in range(a):
+    listTemp =[]
+    for j in range(b):
+        listTemp.append(int(input(f"Ingrese el elemento de la fila {i+1} y la columnas {j+1}: ")))
+    lista2.append(listTemp)
+
+print()"""
+
+#listaUnos =[1]*100             #Vector de datos
+#listaUnos =[[1]*100]*100       #Lista de datos
+listaUnos =[[[1]*100]*100]*    #100 Cubo de datos
+print(listaUnos)      
