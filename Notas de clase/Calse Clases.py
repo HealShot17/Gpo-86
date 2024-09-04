5a4e8508e2e5d3950c7d34c71fd0d0592983746d
HealShot17 <albertokcrs@outlook.com>
2024-09-04 14:48:38 -0500

    Clases

---
+++ Notas de clase/Calse Clases.py
@@ -0,0 +1,28 @@
+'''
+Clases de python
+Clases 
+Declaración
+Uso
+Herencia
+Polimorfismo
+Abstración
+'''
+
+class vehiculo:
+    def __init__(self) -> None:
+    self.Fabricante:
+    self.Anio
+    self.Ruedas
+    self.Volante
+    self.Alas
+    self.Ventanas
+    self.Motor
+    self.Clima
+    self.Kilometraje
+    self.Puertas
+
+
+
+
+
+class nombre:
